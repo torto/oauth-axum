6d490b0ddfd5486bbb362782e846f2e90c3a7002
Guilherme Berghauser <tortoyoyo@gmail.com>
2024-05-10 12:12:54 +0200

    Adjust documentation of microsoft

--- src/providers/microsoft.rs
+++ src/providers/microsoft.rs
@@ -3,6 +3,14 @@ use crate::CustomProvider;
 pub struct MicrosoftProvider {}
 
 impl MicrosoftProvider {
+    /// Create a new MicrosoftProvider
+    ///
+    /// # Arguments
+    /// * `tenant_id` - The tenant id - Check Microsfot docmentation for more information: https://learn.microsoft.com/en-us/entra/identity-platform/v2-oauth2-auth-code-flow#request-an-authorization-code
+    /// * `client_id` - The client id
+    /// * `client_secret` - The client secret
+    /// * `redirect_url` - The redirect url
+    ///
     pub fn new(
         tenant_id: String,
         client_id: String,
